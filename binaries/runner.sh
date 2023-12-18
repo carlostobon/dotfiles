c4cd48a2732d0d82d28eb71f512bd5126b5530eb
xeon <carlostobon@tutanota.de>
2023-12-18 16:51:48 -0500

    add bins runner

--- binaries/runner.sh
+++ binaries/runner.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-# This should be a thing to dowload all personal binaries
+
+bins=("duer" "ww")
+
+for item in "${bins[@]}"; do
+  curl -O "https://server.numericaltools.com/bins/$item" &
+done
+
+wait
+
+for item in "${bins[@]}"; do
+  chmod +x "$item"
+done
