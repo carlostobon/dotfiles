dfa8eefcc7b061378a24a823eee9fbac299d05e2
carlostobon <carlostobon@tutanota.de>
2024-01-06 11:18:05 -0500

    fix xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -207,20 +207,20 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- which denotes layout choice.
 --
 
---myLayout = spacing 10 $ gaps [(U, 10), (R, 30), (D, 10), (L, 30)]
-     --tiled ||| Mirror tiled ||| Full
-  --where
-     ---- default tiling algorithm partitions the screen into two panes
-     --tiled   = Tall nmaster delta ratio
+myLayout = spacing 10 $ gaps [(U, 10), (R, 30), (D, 10), (L, 30)]
+     tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
 
-     ---- The default number of windows in the master pane
-     --nmaster = 1
+     -- The default number of windows in the master pane
+     nmaster = 1
 
-     ---- Default proportion of screen occupied by master pane
-     --ratio   = 1/2
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
 
-     ---- Percent of screen to increment by when resizing panes
-     --delta   = 3/100
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
 
 
 ------------------------------------------------------------------------
@@ -314,7 +314,7 @@ defaults = def {
         mouseBindings      = myMouseBindings,
 
       -- hooks, layouts
-        --layoutHook         = myLayout,
+        layoutHook         = myLayout,
         manageHook         = myManageHook,
         handleEventHook    = myHandleEventHook,
         logHook            = myLogHook,
