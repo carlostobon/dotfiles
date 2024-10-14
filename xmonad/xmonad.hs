f6188aa64e165a07fb98936f672a2bcf9c1e21ff
carlostobon <carlostobon@tutanota.de>
2024-10-14 10:30:42 -0500

    feat: re-write xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,120 +1,97 @@
-import XMonad.Layout.Gaps
 import XMonad
-import Data.Monoid
-import System.Exit
-import XMonad.Hooks.WindowSwallowing
-
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 import XMonad.Actions.CycleWS
+import XMonad.Layout.Gaps
+import XMonad.Layout.Dwindle
 import XMonad.Util.SpawnOnce
 import XMonad.Layout.Spacing
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-import XMonad.Layout.Gaps
-
-myTerminal      = "kitty"
-
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = True
-
--- Whether clicking on a window to focus also passes the click to the window
-myClickJustFocuses :: Bool
-myClickJustFocuses = False
-
--- Width of the window border in pixels.
-myBorderWidth   = 0
-
-myModMask       = mod4Mask
-
-myWorkspaces=["1","2","3","4","5","6","7","8","9","10","11","12","13","14"]
-
--- Border colors for unfocused and focused windows, respectively.
-myNormalBorderColor  = "#d4ccb9"
-myFocusedBorderColor = "#ffffff"
+import XMonad.Hooks.WindowSwallowing
 
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
+-------------------------------------------------------------
+-- Basics
+
+myTerminal            = "kitty"
+myModMask             = mod4Mask
+myFocusFollowsMouse   = True
+myClickJustFocuses    = False
+myBorderWidth         = 0
+myNormalBorderColor   = "#ffffff"
+myFocusedBorderColor  = "#ffffff"
+
+myWorkspaces =
+  [ "1", "2", "3", "4", "5"
+  , "6", "7", "8", "9", "10"
+  , "11", "12", "13", "14"
+  ]
+
+
+myHandleEventHook = swallowEventHook
+    (className =? "kitty")
+    (return True)
+
+-------------------------------------------------------------
+-- Keys:
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-
-    -- Launch a terminal
-    [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
-
-    -- Recompile and Restart xmonad
-    , ((modm,               xK_period ), spawn "xmonad --recompile && xmonad --restart")
-
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_y ), io (exitWith ExitSuccess))
-
-    -- Launch qutebrowser
-    , ((modm,               xK_f ), spawn "qutebrowser")
-
-    -- Launch brave
-    , ((modm,               xK_p ), spawn "brave")
-
-
-
-    -- Simulate copy
-    , ((modm,               xK_k ), spawn "sleep 0.2 && xdotool key ctrl+c")
-
-    -- Simulate paste
-    , ((modm,               xK_h ), spawn "sleep 0.2 && xdotool key ctrl+v")
-
-    -- Simulate scape
-    , ((modm,               xK_apostrophe ), spawn "sleep 0.2 && xdotool key Escape")
-
-    -- Shrink the master area
-    , ((modm,               xK_m ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_o ), sendMessage Expand)
-
-
-
-    -- Move to prev workspace
-    , ((modm,               xK_n ),  moveTo Prev (Not emptyWS))
-
-    -- Move to prev workspace
-    , ((modm,               xK_i ),  moveTo Next (Not emptyWS))
-
-
-    -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
-
-    -- Find an empty workspace
-    , ((modm,               xK_t ), moveTo Next emptyWS)
+  [
+    -- Launch a window
+    ((modm,  xK_Return), spawn $ XMonad.terminal conf),
 
     -- Close focused window
-    , ((modm,               xK_s ), kill)
+    ((modm,  xK_s),  kill),
 
-    -- Move focus to the next window
-    , ((modm,               xK_e     ), windows W.focusDown)
+    -- Focus next window
+    ((modm,  xK_e),  windows W.focusDown),
+
+    -- Move to prev workspace
+    ((modm,  xK_n),  moveTo Prev (Not emptyWS)),
+
+    -- Move to next workspace
+    ((modm,  xK_i),  moveTo Next (Not emptyWS)),
+
+    -- Find an empty workspace
+    ((modm,  xK_t),  moveTo Next emptyWS),
+
+    -- Shrink master area
+    ((modm,  xK_m),  sendMessage Shrink),
+
+    -- Expand master area
+    ((modm,  xK_o),  sendMessage Expand),
+
+    -- Rotate between layouts
+    ((modm,  xK_space), sendMessage NextLayout),
+
+    -- Swap to master window
+    ((modm,  xK_comma ), windows W.swapMaster),
 
 
-    -- Record video
-    --, ((modm .|. shiftMask, xK_y ), spawn "toolbox record-screen")
+    -- Custom keys not related to XMonad
+    --
+    -- Launch qutebrowser
+    ((modm,  xK_f), spawn "qutebrowser"),
 
-    -- Record audio
-    , ((modm .|. shiftMask, xK_a ), spawn "toolbox record-audio")
+    -- Launch brave
+    ((modm,  xK_p), spawn "brave"),
 
-    -- Kill all current recordings
-    , ((modm .|. shiftMask, xK_l ), spawn "killall ffmpeg")
+    -- Emulate copy
+    ((modm,  xK_k), spawn "sleep 0.2 && xdotool key ctrl+c"),
 
-    -- Capture screen
-    , ((modm .|. shiftMask, xK_p ), spawn "toolbox capture-screen")
+    -- Emulate paste
+    ((modm,  xK_h), spawn "sleep 0.2 && xdotool key ctrl+v"),
 
-    -- Swap the focused window and the master window
-    , ((modm,               xK_comma ), windows W.swapMaster)
+    -- Simulate scape
+    ((modm,  xK_apostrophe), spawn "sleep 0.2 && xdotool key Escape")
 
-    ]
+  ]
 
-------------------------------------------------------------------------
 
+-------------------------------------------------------------
 -- Layouts:
 
 myLayout = spacing 10 $ gaps [(U, 10), (R, 30), (D, 10), (L, 30)]
-     tiled ||| Mirror tiled ||| Full
+     (tiled ||| Mirror tiled ||| Full ||| Dwindle L CW (3/2) (11/10))
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -128,15 +105,11 @@ myLayout = spacing 10 $ gaps [(U, 10), (R, 30), (D, 10), (L, 30)]
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
-------------------------------------------------------------------------
 
--- Startup hook
--- Perform an arbitrary action each time xmonad starts or is restarted
+--------------------------------------------------------------
+-- StartupHook
 
 myStartupHook = do
- --spawnOnce "xrandr --output DP-1 --off"
- --spawnOnce "xrandr --output HDMI-A-0 --off"
- --spawnOnce "xrandr --output HDMI-A-0 --mode 1920x1080"
  spawnOnce "picom --config .config/picom/picom.conf"
  spawn "xset r rate 250 80 &"
  spawn "xset s 18000 &"
@@ -144,30 +117,22 @@ myStartupHook = do
  spawn "setxkbmap -option 'caps:super'"
  spawn "xwallpaper --stretch .config/wallpaper/wallpaper.png &"
 
-------------------------------------------------------------------------
 
--- Now run xmonad with all the defaults we set up.
-
-main = xmonad defaults
-
-myHandleEventHook = swallowEventHook (className =? "kitty") (return True)
+--------------------------------------------------------------
+-- Defaults
 
 defaults = def {
-      -- simple stuff
-        terminal           = myTerminal,
-        focusFollowsMouse  = myFocusFollowsMouse,
-        clickJustFocuses   = myClickJustFocuses,
-        borderWidth        = myBorderWidth,
-        modMask            = myModMask,
-        workspaces         = myWorkspaces,
-        normalBorderColor  = myNormalBorderColor,
-        focusedBorderColor = myFocusedBorderColor,
+    terminal           = myTerminal,
+    modMask            = myModMask,
+    workspaces         = myWorkspaces,
+    focusFollowsMouse  = myFocusFollowsMouse,
+    keys               = myKeys,
+    layoutHook         = myLayout,
+    startupHook        = myStartupHook,
+    borderWidth        = myBorderWidth,
+    normalBorderColor  = myNormalBorderColor,
+    handleEventHook    = myHandleEventHook,
+    focusedBorderColor = myFocusedBorderColor
+}
 
-      -- key bindings
-        keys               = myKeys,
-
-      -- hooks, layouts
-        layoutHook         = myLayout,
-        handleEventHook    = myHandleEventHook,
-        startupHook        = myStartupHook
-    }
+main = xmonad defaults
